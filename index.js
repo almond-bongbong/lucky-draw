9a41a2d52ac34bf0f5712aedf8f5fead03f498f1
choongman <maxx@encar.com>
2023-05-12 17:26:34 +0900

    :zap: update anim

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function init() {
 }
 
 function createRing() {
-	const PARTICLE_COUNT = 5000;
+	const PARTICLE_COUNT = 1;
 	for (let i = 0; i < PARTICLE_COUNT; i++) {
 		particles.push(new Particle());
 	}
@@ -46,16 +46,36 @@ function render() {
 			const p = particles[i];
 			p.update();
 			p.draw(ctx);
-			if (p.opacity <= 0 || p.x > canvasWidth || p.y > canvasHeight) {
-				particles.splice(particles.indexOf(p), 1);
-				particles.push(new Particle());
-			}
 		}
 
 		then = now - (delta % interval);
 	};
 
 	requestAnimationFrame(frame);
+}
+
+function getTextPixels(text, fontSize, fontFamily) {
+	const offscreenCanvas = document.createElement('canvas');
+	offscreenCanvas.width = canvas.width;
+	offscreenCanvas.height = canvas.height;
+
+	const offscreenCtx = offscreenCanvas.getContext('2d');
+	offscreenCtx.font = `${fontSize}px ${fontFamily}`;
+	offscreenCtx.fillText(text, canvas.width / 2, canvas.height / 2);
+
+	const imageData = offscreenCtx.getImageData(0, 0, canvas.width, canvas.height);
+	const pixels = [];
+
+	for(let y = 0; y < canvas.height; y++) {
+		for(let x = 0; x < canvas.width; x++) {
+			const alpha = imageData.data[(y * canvas.width + x) * 4 + 3];
+			if(alpha > 0) {
+				pixels.push({ x, y });
+			}
+		}
+	}
+
+	return pixels;
 }
 
 window.addEventListener('load', () => {
@@ -66,7 +86,10 @@ window.addEventListener('load', () => {
 
 window.addEventListener('resize', init);
 window.addEventListener('click', () => {
-	// const ring = document.querySelector('.ring');
-	// ring.classList.add('fade-out');
-	// particles.forEach((p) => p.start());
+	const textPixels = getTextPixels('10289', 70, 'Arial');
+	for(let i = 0; i < particles.length; i++) {
+		const p = particles[i];
+		const targetPixel = textPixels[i % textPixels.length];
+		p.setTarget(targetPixel.x, targetPixel.y);
+	}
 });
--- js/Particle.js
+++ js/Particle.js
@@ -1,43 +1,52 @@
 import { randomNumBetween } from './utils.js';
 
 class Particle {
-	constructor() {
-		this.radius = innerHeight / 4;
-		this.theta = randomNumBetween(0, Math.PI * 2);
-		this.radiusFriction = 0;
-		this.thetaFriction = 0;
-		this.radiusAlpha = 0;
-		this.thetaAlpha = 0;
-		this.opacity = 0;
-		this.maxOpacity = randomNumBetween(0.4, 1);
-		this.x = randomNumBetween(0, window.innerWidth);
-		this.y = randomNumBetween(0, window.innerHeight);
-	}
+  constructor() {
+    this.opacity = randomNumBetween(0.4, 1);
+    this.x = randomNumBetween(0, window.innerWidth);
+    this.y = randomNumBetween(0, window.innerHeight);
+    this.xAlpha = randomNumBetween(-0.5, 0.5);
+    this.yAlpha = randomNumBetween(-0.5, 0.5);
+  }
 
-	start() {
-		this.radiusFriction = randomNumBetween(0.95, 1.01);
-		this.thetaFriction = randomNumBetween(0.97, 0.99);
-		this.radiusAlpha = randomNumBetween(0, 4);
-		this.thetaAlpha = randomNumBetween(0.02, 0.04);
-	}
+  update() {
+    this.x += this.xAlpha;
+    this.y += this.yAlpha;
 
-	update() {
-		this.radiusAlpha *= this.radiusFriction;
-		this.thetaAlpha *= this.thetaFriction;
-		this.radius += this.radiusAlpha;
-		this.theta += this.thetaAlpha;
-		this.x += Math.cos(Math.PI - this.theta);
-		this.y += Math.sin(Math.PI - this.theta);
-		this.opacity = Math.min(this.maxOpacity , this.opacity + 0.01);
-	}
+    if (this.x < -100 || this.x > window.innerWidth + 100) {
+      this.xAlpha = this.xAlpha * -1;
+    }
+    if (this.y < -100 || this.y > window.innerHeight + 100) {
+      this.yAlpha = this.yAlpha * -1;
+    }
 
-	draw(ctx) {
-		ctx.beginPath();
-		ctx.arc(this.x, this.y, 1, 0, (Math.PI / 180) * 360);
-		ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
-		ctx.fill();
-		ctx.closePath();
-	}
+    if (this.targetX !== undefined && this.targetY !== undefined) {
+      // Move towards target
+      const dx = this.targetX - this.x;
+      const dy = this.targetY - this.y;
+      // const dist = Math.sqrt(dx * dx + dy * dy);
+      const velX = dx;
+      const velY = dy;
+      const speed = 1; // adjust this value to change the speed
+      this.x += velX * speed;
+      this.y += velY * speed;
+
+      console.log(velX, speed);
+    }
+  }
+
+  setTarget(x, y) {
+    this.targetX = x;
+    this.targetY = y;
+  }
+
+  draw(ctx) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 1, 0, (Math.PI / 180) * 360);
+    ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
+    ctx.fill();
+    ctx.closePath();
+  }
 }
 
 export default Particle;
